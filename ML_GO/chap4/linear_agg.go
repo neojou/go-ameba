2b78ec7e1a644ab01d71b1fa3d5f34cfb994afa0
neo_jou <neo_jou@realtek.com>
2019-11-21 10:28:28 +0800

    ML_GO : ch4 	linear_agg.go
    tGOpl : ch4
    	month.go
    	sha256.go
    
    Signed-off-by: neo_jou <neo_jou@realtek.com>

--- ML_GO/chap4/linear_agg.go
+++ ML_GO/chap4/linear_agg.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"log"
 	"github.com/kniren/gota/dataframe"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 type CSVRecord struct {
@@ -25,8 +27,34 @@ func main() {
 
 	advertDF := dataframe.ReadCSV(advertFile)
 
-	advertSummary := advertDF.Describe()
+	yVals := advertDF.Col("Sales").Float()
 
-	fmt.Println(advertSummary)
+	for _, colName := range advertDF.Names() {
+		pts := make(plotter.XYs, advertDF.Nrow())
+
+		for i, floatVal := range advertDF.Col(colName).Float() {
+			pts[i].X = floatVal
+			pts[i].Y = yVals[i]
+		}
+
+		p, err := plot.New()
+		if err != nil {
+			log.Fatal(err)
+		}
+		p.X.Label.Text = colName
+		p.Y.Label.Text = "y"
+		p.Add(plotter.NewGrid())
+
+		s, err := plotter.NewScatter(pts)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s.GlyphStyle.Radius = vg.Points(3)
+
+		p.Add(s)
+		if err = p.Save(4*vg.Inch, 4*vg.Inch, colName+"_scatter.png"); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
